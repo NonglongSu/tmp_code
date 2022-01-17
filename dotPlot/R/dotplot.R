30cdb7cdfd123d58ecee14e402d91f3f6bb78694
Juan J. Garcia Mesa <juanjosegarciamesa@gmail.com>
2022-01-17 15:51:19 -0700

    Add matrix to dataframe function

--- dotPlot/R/dotplot.R
+++ dotPlot/R/dotplot.R
@@ -6,7 +6,7 @@
 generate_dot_matrix = function(input){
   #read input
   aln = read_input(input)
-  
+
   #initialize matrix
   mat = 0
   for(i in seq_len(nrow(aln))) {
@@ -18,8 +18,8 @@ generate_dot_matrix = function(input){
 
 #' This function plots a dot matrix as a raster image.
 #'
-#' @param  filepath:    either a directory contains all alignments (DNA/AA), or a json fil contains all alignments of a single raw DNA/AA sequence.
-#' @param  use_ggplot: `TRUE` or `FALSE`. It is false by default. Otherwise, setup use_ggplot=TRUE which returns a raster ggplot. 
+#' @param  filepath:    either a directory contains all alignments (DNA/AA), or a json file contains all alignments of a single raw DNA/AA sequence.
+#' @param  use_ggplot: `TRUE` or `FALSE`. It is false by default. Otherwise, setup use_ggplot=TRUE which returns a raster ggplot.
 #' @return A raster plot.
 #' @export
 plot_dot_matrix = function(input,use_ggplot=FALSE){
@@ -29,9 +29,31 @@ plot_dot_matrix = function(input,use_ggplot=FALSE){
   create_plot(mat, aln, use_ggplot)
 }
 
-#generate a data.frame (need to consult reed!)
+#' Produce a data frame given a dot matrix.
+#'
+#' @param mat: dot matrix.
+#' @return a data frame.
+#' @export
+matrix_to_dataframe <- function(mat) {
+  nrows = nrow(mat)
+  ncols = ncol(mat)
+  len = length(mat)
 
+  # coordinates for seq1 & seq2
+  #   seq1: increments of 0.5, each repeated by number of columns
+  #   seq2: increments of 0.5, repeated overall by number of rows
+  seq1 <- seq(from = 0.5, by = 0.5, length.out = nrows)
+  seq1 <- rep(seq1, each = ncols)
+  seq2 <- seq(from = 0.5, by = 0.5, length.out = ncols)
+  seq2 <- rep(seq2, times = nrows)
 
+  # magnitude
+  magnitude <- as.vector(mat)
+
+  # data frame
+  data <- data.frame(Seq1 = seq1, Seq2 = seq2, Magnitude = magnitude)
+  data
+}
 
 
 ################################################################################
@@ -74,11 +96,11 @@ create_plot = function(mat,aln,use_ggplot) {
     abm = cbind(abm,c(t(mat)))
     dmat = data.frame(abm)
     colnames(dmat)=c('Seq1','Seq2','Magnitude')
-    
+
     ggplot(dmat)+ geom_raster(mapping=aes(x=Seq1, y=Seq2, fill=Magnitude)) +
       scale_fill_gradientn(colours= rev(terrain.colors(10)), name='Magnitude')
    }
-  
+
 }
 
 
@@ -86,45 +108,45 @@ create_plot = function(mat,aln,use_ggplot) {
 #read the DNA/AA alignments from a file or directory
 read_input = function(input_list) {
   #define data frame
-  data = data.frame(Seq1 = character(), Seq2 = character())              
-  
+  data = data.frame(Seq1 = character(), Seq2 = character())
+
   #if only one argument and is a dir, look for all fasta files inside dir
   if(dir.exists(input_list)) {
-    fasta_list = read_dir(input_list)                                   
+    fasta_list = read_dir(input_list)
     if(length(fasta_list) > 0) {
       input_list = fasta_list
     } else {
       stop("No fasta files found in current directory.")
     }
   }
-  
+
   #read json files
-  for(input in input_list) {                                           
+  for(input in input_list) {
     if(tools::file_ext(input) == "fasta" || tools::file_ext(input) == "fa") {
       # read fasta, concatenate to data frame
       data[nrow(data)+1, ] = read_fasta(input)
     } else if(tools::file_ext(input) == "json") {
       # read json, concatenate to data frame
-      data = rbind(data, read_json(input))                           
+      data = rbind(data, read_json(input))
     } else {
       stop("Input format not supported.")
     }
   }
-  
+
   #count unique alignments and return data frame
-  data = data %>% dplyr::count(Seq1, Seq2)                            
+  data = data %>% dplyr::count(Seq1, Seq2)
   data
 }
 
 #find all fasta files in `dir_path` and return a list of files
-read_dir = function(dir_path) {    
+read_dir = function(dir_path) {
   files = list.files(path = dir_path, pattern = "\\.fasta$", full.names = TRUE)
   files = append(files, list.files(path = dir_path, pattern = "\\.fa$", full.names = TRUE))
   files
 }
 
 #read json file (coati-sample output format) and return as data frame
-read_json = function(json_path) { 
+read_json = function(json_path) {
     data = jsonlite::fromJSON(json_path)
     data = data %>% tidyr::unpack(aln)
     data
